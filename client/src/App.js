45c8f58fc6ae9c587236c3154b0aa556b589bb77
Mister M Codes <mister.m.codes@gmail.com>
2020-08-10 14:50:35 +0800

    added padding

--- client/src/App.js
+++ client/src/App.js
@@ -11,12 +11,12 @@ class App extends Component {
       <Router>
         <div className="App">
           <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
+            <img src={logo} className="App-logo" alt="logo" />            
+            <h1 className="App-title">Fib Calculator</h1>
             <Link to="/">Home</Link>
-            <Link to="/otherpage">Other Page</Link>
+            <Link to="/otherpage">Other Page</Link>            
           </header>
-          <div>
+          <div style="margin-top:20px;">
             <Route exact path="/" component={Fib} />
             <Route path="/otherpage" component={OtherPage} />
           </div>
